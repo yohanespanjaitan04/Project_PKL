4fd34e1a81649cca6028dfaaf21369ba7bb440db
rayyis <rayisbudi64@gmail.com>
2025-05-20 21:45:28 +0700

    logo sudah

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -19,7 +19,7 @@
             </div>
         </div>
         <div class="user-info">
-            <svg class="user-icon" xmlns="http://www.w3.org/2000/svg" fill="none"
+           <img src="{{ url('image/undip.png') }}" alt="Gambar">
                  viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
